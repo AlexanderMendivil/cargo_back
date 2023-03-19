4ff32668e81bd22454867391de39c804e2164b58
Alexander Mendivil <alexanderima@outlook.es>
2023-03-19 14:53:55 -0700

    relacion en la query sql

--- DBConfig/index.ts
+++ DBConfig/index.ts
@@ -1,7 +1,11 @@
+import { VehicleInterface } from "../models/VehicleModel";
 import { executeQuery } from "../utils/query"
 
 export const getDailyEvent = async (date: string) => {
-   return await executeQuery(`SELECT * FROM checklist WHERE CAST(fecha AS date) = '${date}'`);
+   return await executeQuery(`select cat_eqmovil.tipo_equipo, cat_eqmovil.estado_equipo, checklist.* 
+   FROM cat_eqmovil JOIN checklist ON cat_eqmovil.equipo_movil = checklist.equipomovil
+   WHERE CAST(checklist.fecha AS date) = '${date}';`) as VehicleInterface[];
+   
 }
 
   
