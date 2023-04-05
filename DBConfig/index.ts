21eb9c588e83466c447a79cff0f8251dfda544e5
Alexander Mendivil <alexanderima@outlook.es>
2023-04-04 18:29:59 -0700

    quitar comentario

--- DBConfig/index.ts
+++ DBConfig/index.ts
@@ -18,11 +18,6 @@ export const getDailyEvent = async (date: string) => {
    }
 
    return vehicles;
-   
-   // return await executeQuery(`select cat_eqmovil.tipo_equipo, cat_eqmovil.estado_equipo, checklist.* 
-   // FROM cat_eqmovil JOIN checklist ON cat_eqmovil.equipo_movil = checklist.equipomovil
-   // WHERE CAST(checklist.fecha AS date) = '${date}';`) as VehicleInterface[];
-   
 }
 
   
