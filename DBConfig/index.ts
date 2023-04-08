956c1e2b43ffc70ba18168449f0f86f9c9856222
Alexander Mendivil <alexanderima@outlook.es>
2023-04-07 17:34:17 -0700

    filtro de vehiculos funcionando

--- DBConfig/index.ts
+++ DBConfig/index.ts
@@ -18,38 +18,51 @@ export const getDailyEvent = async (date: string) => {
    const checkList = await executeQuery(`select * from checklist WHERE CAST(checklist.fecha AS date) = '${date}';`) as CheckListModel[];
    const catMovilList = await executeQuery(`select  * from cat_eqmovil WHERE CAST(cat_eqmovil.fecha_alta AS date) = '${date}';`) as CatEquipoModel[];
 
-   const unfilterVehicles: Vehicle[] = [];
+   const inactiveVehicles: Vehicle[] = [];
+   const activeVehicles: Vehicle[] = [];
    for(let i = 0; i < catMovilList.length; i++){
 
       if(checkList.length > 0){
          for(let j = 0; j < checkList.length; j++){
             if(catMovilList[i].equipo_movil === checkList[j]?.equipomovil && moment(catMovilList[i].fecha_alta).format('YYYY-MM-D') === moment(checkList[j]?.fecha).format('YYYY-MM-D') && catMovilList[i].area === checkList[j]?.area ){
-               unfilterVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: checkList[j].area, employee: checkList[j].nombre, status: true, identifier: uuidv4() });
+               activeVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: checkList[j].area, employee: checkList[j].nombre, status: true, identifier: uuidv4() });
                break;
             }else{
-               unfilterVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: catMovilList[i].area, employee: '---', status: false, identifier: uuidv4() });
+               if(activeVehicles.some(vehicle => vehicle.vehicleId !== catMovilList[i].equipo_movil))
+                  inactiveVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: catMovilList[i].area, employee: '---', status: false, identifier: uuidv4() });
             }  
          }
       }else{
-         unfilterVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: catMovilList[i].area, employee: '---', status: false, identifier: uuidv4() });
+         inactiveVehicles.push({ id: catMovilList[i].id, vehicleType: catMovilList[i].tipo_equipo, vehicleId: catMovilList[i].equipo_movil, area: catMovilList[i].area, employee: '---', status: false, identifier: uuidv4() });
 
       }
    }
 
-   for(let i = 0; i < unfilterVehicles.length; i++){
-      for(let j = 0; j < unfilterVehicles.length -1; j++){
-         if(unfilterVehicles[i].id === unfilterVehicles[j].id && unfilterVehicles[i].vehicleId === unfilterVehicles[j].vehicleId && unfilterVehicles[i].vehicleType === unfilterVehicles[j].vehicleType && unfilterVehicles[i].area === unfilterVehicles[j].area && unfilterVehicles[i].status !== unfilterVehicles[j].status){
-            if(unfilterVehicles[i].status){
-               unfilterVehicles.splice(j,1)
-            }else if(unfilterVehicles[j].status)
-               unfilterVehicles.splice(i,1)
-         }else if( unfilterVehicles[i].id === unfilterVehicles[j].id && unfilterVehicles[i].vehicleId === unfilterVehicles[j].vehicleId && unfilterVehicles[i].vehicleType === unfilterVehicles[j].vehicleType && unfilterVehicles[i].area === unfilterVehicles[j].area && unfilterVehicles[i].status === unfilterVehicles[j].status && unfilterVehicles[i].identifier !== unfilterVehicles[j].identifier){
-            unfilterVehicles.splice(j,1)
+   const duplicatedVehicles: Vehicle[] = [];
+   for(let i = 0; i <= inactiveVehicles.length-1; i++){
+      for(let j = 0; j < activeVehicles.length; j++){
+         if(inactiveVehicles[i].id === activeVehicles[j].id && inactiveVehicles[i].vehicleId === activeVehicles[j].vehicleId && inactiveVehicles[i].vehicleType === activeVehicles[j].vehicleType && inactiveVehicles[i].area === activeVehicles[j].area){
+            duplicatedVehicles.push(inactiveVehicles[i]);
+         }
+      }
+   }
+
+   const filteredDuplicatedVehicles = inactiveVehicles.filter(vehicle => !duplicatedVehicles.includes(vehicle));
+
+   const cleanedInactiveVehicles: Vehicle[] = [];
+   for(let i = 0; i < filteredDuplicatedVehicles.length; i++){
+      for(let j = 0; j < filteredDuplicatedVehicles.length -1; j++){
+         if( filteredDuplicatedVehicles[i].id === filteredDuplicatedVehicles[j].id && filteredDuplicatedVehicles[i].vehicleId === filteredDuplicatedVehicles[j].vehicleId && filteredDuplicatedVehicles[i].vehicleType === filteredDuplicatedVehicles[j].vehicleType && filteredDuplicatedVehicles[i].area === filteredDuplicatedVehicles[j].area && filteredDuplicatedVehicles[i].status === filteredDuplicatedVehicles[j].status && filteredDuplicatedVehicles[i].identifier !== filteredDuplicatedVehicles[j].identifier){
+            if(cleanedInactiveVehicles.some(vehicle => vehicle.vehicleType === filteredDuplicatedVehicles[j].vehicleType)){
+               break;   
+            }
+            cleanedInactiveVehicles.push(filteredDuplicatedVehicles[j]);
+            break;
 
          }
       }
    }
-   return unfilterVehicles;
+   return [...activeVehicles, ...cleanedInactiveVehicles];
 }
 
   
